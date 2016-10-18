60168a7430d1e0003f82385888d320122a986aed
Wilson Martin <wilson.martin@gtri.gatech.edu>
2016-10-18 13:02:09 -0400

    updated script for comparing vcf to clinical data

--- compare_clin_with_vcf.py
+++ compare_clin_with_vcf.py
@@ -6,21 +6,27 @@ import vcf
 cache = defaultdict(lambda: defaultdict(list))
 patho = {}
 
-for row in csv.DictReader(open(sys.argv[1])):
-    patho_key = row['Genomic_Coordinate_hg37']
-    if patho_key[0:3] == 'chr':
-        patho_key = patho_key[3:]
+if len(sys.argv) < 3:
+    print("usage: {} file.vcf file1.csv file2.csv ...".format(sys.argv[0]))
+    exit(1)
 
-    patho[patho_key] = row['Pathogenicity_research']
-    recs = patho_key.split(':')
-    cache[recs[0]][recs[1]].append(recs[2])
+for fn in sys.argv[2:]:
+    for row in csv.DictReader(open(fn)):
+        patho_key = row['Genomic_Coordinate_hg37']
+        if patho_key[0:3] == 'chr':
+            patho_key = patho_key[3:]
+        if row['Pathogenicity_research'] != "":
+            patho[patho_key] = row['Pathogenicity_research']
+        recs = patho_key.split(':')
+        cache[recs[0]][recs[1]].append(recs[2])
 
-for rec in vcf.Reader(open(sys.argv[2])):
-    for alt in rec.ALT:
-        change = rec.REF + '>' + alt
-        if change in cache[rec.CHROM][rec.POS]:
-            path_key = ':'.join([rec.CHROM, rec.POS, change])
-            if path_key in patho:
-                print(rec, patho[path_key])
-            else:
-                print(rec, 'no pathogenicity info')
+for rec in vcf.Reader(open(sys.argv[1])):
+    chrom = rec.CHROM[3:] if rec.CHROM[0:3] == 'chr' else str(rec.CHROM)
+    if chrom in cache:
+        pos = str(rec.POS)
+        for alt in map(str, rec.ALT):
+            change = rec.REF + '>' + alt
+            if change in cache[chrom][pos]:
+                path_key = ':'.join([chrom, pos, change])
+                if path_key in patho and patho[path_key]:
+                    print(path_key + '\t' + patho[path_key])
